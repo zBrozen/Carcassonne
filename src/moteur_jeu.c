be28e4574c3a8587d2cabf07f1a829ea8792babd
zBrozen <babealexandre409@gmail.com>
2024-02-02 10:19:52 +0100

    Test

---
+++ src/moteur_jeu.c
@@ -0,0 +1,21 @@
+#include "game_structures.h"
+#include "lecteur_csv.h"
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+    struct tuile_s t;
+
+    printf("\033[41m \033[0m");
+    t->identifiant = 0;
+    t->cotes[0] = "R";
+    t->cotes[1] = "V";
+    t->cotes[2] = "R";
+    t->cotes[3] = "P";
+    
+
+
+    //parseur_csv();
+
+    return 0;
+}
